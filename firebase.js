a45ef3d773cf3f8d284731304ebf21f7ea7ceb7c
SSudharsan876 <sudharsansankar739@gmail.com>
2026-08-13 00:17:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,11 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBbMJrfjqjk_9mRtoMi1lEQoTbRkeQyYlE",
   authDomain: "prompt-forge-5da75.firebaseapp.com",
   projectId: "prompt-forge-5da75",
-  storageBucket: "prompt-forge-5da75.firebasestorage.app",
+  storageBucket: "prompt-forge-5da75.appspot.com",
   messagingSenderId: "1037395691833",
-  appId: "1:1037395691833:web:4f1d3e110fec1df9e280e8"
+  appId: "1:1037395691833:web:4f1d3e110fec1df9e280e8",
+  measurementId: "G-GRTTJVWMJ3"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
