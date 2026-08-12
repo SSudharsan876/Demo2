d7a08e4a40850d6a011c96d0238cb8e9adaaaa1f
SSudharsan876 <sudharsansankar739@gmail.com>
2026-08-12 23:49:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbMJrfjqjk_9mRtoMi1lEQoTbRkeQyYlE",
@@ -12,4 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const storage = getStorage(app);
