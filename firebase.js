afa9cd7592b30eb15c9afba032e7c472d8bc137f
SSudharsan876 <sudharsansankar739@gmail.com>
2026-09-01 12:27:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -17,13 +17,13 @@ import { getAuth } from
 // =====================================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBbMJrfjqjk_9mRtoMi1lEQoTbRkeYlE",
-    authDomain: "prompt-forge-5da75.firebaseapp.com",
-    projectId: "prompt-forge-5da75",
-    storageBucket: "prompt-forge-5da75.firebasestorage.app",
-    messagingSenderId: "1037395691833",
-    appId: "1:1037395691833:web:4f1d3e110fec1df9e280e8",
-    measurementId: "G-GRTTJVWMJ3"
+  apiKey: "AIzaSyBbMJrfjqjk_9mRtoMi1lEQoTbRkeQyYlE",
+  authDomain: "prompt-forge-5da75.firebaseapp.com",
+  projectId: "prompt-forge-5da75",
+  storageBucket: "prompt-forge-5da75.firebasestorage.app",
+  messagingSenderId: "1037395691833",
+  appId: "1:1037395691833:web:4f1d3e110fec1df9e280e8",
+  measurementId: "G-GRTTJVWMJ3"
 };
 
 
